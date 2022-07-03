2fff6db8c30677d9346ccf7e7e8d58278a4a99ac
Dannel Albert <cuebix@gmail.com>
2022-07-03 17:01:01 -0400

    fix: rendering CSFv3 stories

--- renderers/solid-js/src/public-types.ts
+++ renderers/solid-js/src/public-types.ts
@@ -4,6 +4,7 @@ import type {
   StoryAnnotations,
   AnnotatedStoryFn,
 } from '@storybook/csf';
+import { Component, ComponentProps } from 'solid-js';
 import { SolidFramework } from './types';
 
 export type { Args, ArgTypes, Parameters, StoryContext } from '@storybook/csf';
@@ -35,3 +36,46 @@ export type StoryObj<TArgs = Args> = StoryAnnotations<SolidFramework, TArgs>;
  * @see [Named Story exports](https://storybook.js.org/docs/formats/component-story-format/#named-story-exports)
  */
 export type Story<TArgs = Args> = StoryObj<TArgs>;
+
+/**
+ * For the common case where a component's stories are simple components that receives args as props:
+ *
+ * ```tsx
+ * export default { ... } as ComponentMeta<typeof Button>;
+ * ```
+ */
+export type ComponentMeta<T extends Component> = Meta<ComponentProps<T>>;
+
+/**
+ * For the common case where a (CSFv2) story is a simple component that receives args as props:
+ *
+ * ```tsx
+ * const Template: ComponentStoryFn<typeof Button> = (args) => <Button {...args} />
+ * ```
+ */
+export type ComponentStoryFn<T extends Component> = StoryFn<ComponentProps<T>>;
+
+/**
+ * For the common case where a (CSFv3) story is a simple component that receives args as props:
+ *
+ * ```tsx
+ * const MyStory: ComponentStoryObj<typeof Button> = {
+ *   args: { buttonArg1: 'val' },
+ * }
+ * ```
+ */
+export type ComponentStoryObj<T extends Component> = StoryObj<ComponentProps<T>>;
+
+/**
+ * For the common case where a (CSFv3) story is a simple component that receives args as props.
+ * It can either be a CSFv3 object, or CSVv2 function...
+ *
+ * ```tsx
+ * const MyObjStory: ComponentStory<typeof Button> = {
+ *   args: { buttonArg1: 'val' },
+ * }
+ *
+ * const MyFnStory: ComponentStory<typeof Button> = (args) => <Button {...args} />
+ * ```
+ */
+export type ComponentStory<T extends Component> = ComponentStoryObj<T> | ComponentStoryFn<T>;
